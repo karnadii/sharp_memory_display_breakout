2d448e5fef4b147eb0592658c53e0a4938047c8a
Ujang Karnadi <uk9313285@gmail.com>
2022-08-06 04:19:21 +0700

    update readme and arduino code

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
-# LS011B7DH03 Sharp memory display breakout
-LS011B7DH03 https://www.aliexpress.com/item/1005001809102193.html?spm=a2g0s.9042311.0.0.536c4c4dqYRTEv
+# LS011B7DH03 Sharp memory display breakout 1.1 inch 160x68
+everything is tested and work without problem. 5 PCBs with assembled component is $16 and 5 pcs [LS011B7DH03](https://www.aliexpress.com/item/1005001809102193.html) is $75 ($15x5), so for one LS011B7DH03 Sharp memory it cost about $18.2 before shipping.
 
 ![](https://i.imgur.com/EkvLsx7.jpeg)
 ![](https://i.imgur.com/TXG6VWD.jpeg)
@@ -10,11 +10,9 @@ here is a comparison between [nrfmicro](https://github.com/joric/nrfmicro)(promi
 
 ![](https://cdn.discordapp.com/attachments/920911115414814751/921093724509962290/IMG_20210911_230619.jpg)
 
-I have a problem with the fpc connector, the connections seems loose, try using another fpc connector bom. or if you good at soldering, solder directly the lcd to pcb.
-
 I intent to use this breakout as soldered or socketed using pin header or as a daughterboard. if using as a daughter-board don't solder the pin header and solder the jst connector. if soldered/socketed just solder the pin header and leave the jst connector, you can break the mounting hole if you want to save some space,the mountung hole is intended for using it as a daughter-board.
 
-This is the first time I design a pcb just simply looking at datasheet only (the adafruit datasheet and pcb file help me a lot). But well it is running. I am too worried with one different capacitor I use from datasheet, but looking at the adafaruit datasheet using different capacitor value too it seems the datasheet just set a minimal value to use and the value is not absolute. I am worried too much
+This is the first time I design a pcb just simply looking at datasheet only (the adafruit datasheet and pcb file help me a lot). But, well, it is running. I am too worried with one different capacitor I use from datasheet, but looking at the adafaruit datasheet using different capacitor value too it seems the datasheet just set a minimal value to use and the value is not absolute.
 
 running from arduino
 
@@ -25,5 +23,3 @@ running from circuitpython
 https://user-images.githubusercontent.com/18657277/146429499-8556456f-ffeb-47a3-a99c-73e11da73714.mp4
 
 ![https://ae01.alicdn.com/kf/U244e8d4d1993471c84f89b29de7788600.jpg](https://ae01.alicdn.com/kf/U244e8d4d1993471c84f89b29de7788600.jpg)
-
-
---
+++ arduino/sharpmemtest.ino
@@ -0,0 +1,246 @@
+/*********************************************************************
+This is an example sketch for our Monochrome SHARP Memory Displays
+
+  Pick one up today in the adafruit shop!
+  ------> http://www.adafruit.com/products/1393
+
+These displays use SPI to communicate, 3 pins are required to
+interface
+
+Adafruit invests time and resources providing this open source code,
+please support Adafruit and open-source hardware by purchasing
+products from Adafruit!
+
+Written by Limor Fried/Ladyada  for Adafruit Industries.
+BSD license, check license.txt for more information
+All text above, must be included in any redistribution
+*********************************************************************/
+
+#include <Adafruit_GFX.h>
+#include <Adafruit_SharpMem.h>
+
+// Tested using nrfmicro edit your pin according to your controller
+
+// any pins can be used
+#define SHARP_SCK  45 //CLK
+#define SHARP_MOSI 43 //DI
+#define SHARP_SS   10//CS
+
+// nrfmicro pin (32*pin)+gpio
+
+// Set the size of the display here, e.g. 144x168!
+Adafruit_SharpMem display(SHARP_SCK, SHARP_MOSI, SHARP_SS, 160, 68);
+// The currently-available SHARP Memory Display (144x168 pixels)
+// requires > 4K of microcontroller RAM; it WILL NOT WORK on Arduino Uno
+// or other <4K "classic" devices!  The original display (96x96 pixels)
+// does work there, but is no longer produced.
+
+#define BLACK 0
+#define WHITE 1
+
+int minorHalfSize; // 1/2 of lesser of display width or height
+
+void setup(void)
+{
+  Serial.begin(9600);
+  Serial.println("Hello!");
+
+  // start & clear the display
+  display.begin();
+  display.clearDisplay();
+
+  // Several shapes are drawn centered on the screen.  Calculate 1/2 of
+  // lesser of display width or height, this is used repeatedly later.
+  minorHalfSize = min(display.width(), display.height()) / 2;
+
+  // draw a single pixel
+  display.drawPixel(10, 10, BLACK);
+  display.refresh();
+  delay(500);
+  display.clearDisplay();
+
+  // draw many lines
+  testdrawline();
+  delay(500);
+  display.clearDisplay();
+
+  // draw rectangles
+  testdrawrect();
+  delay(500);
+  display.clearDisplay();
+
+  // draw multiple rectangles
+  testfillrect();
+  display.refresh();
+  delay(500);
+  display.clearDisplay();
+
+  // draw a circle, 10 pixel radius
+  display.fillCircle(display.width()/2, display.height()/2, 10, BLACK);
+  display.refresh();
+  delay(500);
+  display.clearDisplay();
+
+  testdrawroundrect();
+  display.refresh();
+  delay(500);
+  display.clearDisplay();
+
+  testfillroundrect();
+  display.refresh();
+  delay(500);
+  display.clearDisplay();
+
+  testdrawtriangle();
+  display.refresh();
+  delay(500);
+  display.clearDisplay();
+
+  testfilltriangle();
+  display.refresh();
+  delay(500);
+  display.clearDisplay();
+
+  testdrawchar();
+  display.refresh();
+  for(int i=0; i<4; i++) {
+    display.refresh();
+    delay(500);
+  }
+}
+
+void loop(void) 
+{
+  for(int i=0; i<4; i++) {
+    display.setRotation(i);
+    display.clearDisplay();
+    // text display tests
+    display.setTextSize(1);
+    display.setTextColor(BLACK);
+    display.setCursor(0,0);
+    display.println("Hello, world!");
+    display.setTextColor(WHITE, BLACK); // inverted text
+    display.println(3.141592);
+    display.setTextSize(2);
+    display.setTextColor(BLACK);
+    display.print("0x"); display.println(0xDEADBEEF, HEX);
+    // Screen must be refreshed at least once per second
+    for(int j=0; j<4; j++) {
+      display.refresh();
+      delay(500); // 1/2 sec delay
+    } // x4 = 2 second pause between rotations
+  }
+}
+
+///
+
+void testdrawline() {
+  for (int i=0; i<display.width(); i+=4) {
+    display.drawLine(0, 0, i, display.height()-1, BLACK);
+    display.refresh();
+  }
+  for (int i=0; i<display.height(); i+=4) {
+    display.drawLine(0, 0, display.width()-1, i, BLACK);
+    display.refresh();
+  }
+  delay(250);
+
+  display.clearDisplay();
+  for (int i=0; i<display.width(); i+=4) {
+    display.drawLine(0, display.height()-1, i, 0, BLACK);
+    display.refresh();
+  }
+  for (int i=display.height()-1; i>=0; i-=4) {
+    display.drawLine(0, display.height()-1, display.width()-1, i, BLACK);
+    display.refresh();
+  }
+  delay(250);
+
+  display.clearDisplay();
+  for (int i=display.width()-1; i>=0; i-=4) {
+    display.drawLine(display.width()-1, display.height()-1, i, 0, BLACK);
+    display.refresh();
+  }
+  for (int i=display.height()-1; i>=0; i-=4) {
+    display.drawLine(display.width()-1, display.height()-1, 0, i, BLACK);
+    display.refresh();
+  }
+  delay(250);
+
+  display.clearDisplay();
+  for (int i=0; i<display.height(); i+=4) {
+    display.drawLine(display.width()-1, 0, 0, i, BLACK);
+    display.refresh();
+  }
+  for (int i=0; i<display.width(); i+=4) {
+    display.drawLine(display.width()-1, 0, i, display.height()-1, BLACK);
+    display.refresh();
+  }
+  delay(250);
+}
+
+void testdrawrect(void) {
+  for (int i=0; i<minorHalfSize; i+=2) {
+    display.drawRect(i, i, display.width()-2*i, display.height()-2*i, BLACK);
+    display.refresh();
+  }
+}
+
+void testfillrect(void) {
+  uint8_t color = BLACK;
+  for (int i=0; i<minorHalfSize; i+=3) {
+    // alternate colors
+    display.fillRect(i, i, display.width()-i*2, display.height()-i*2, color&1);
+    display.refresh();
+    color++;
+  }
+}
+
+void testdrawroundrect(void) {
+  for (int i=0; i<minorHalfSize/2; i+=2) {
+    display.drawRoundRect(i, i, display.width()-2*i, display.height()-2*i, minorHalfSize/2, BLACK);
+    display.refresh();
+  }
+}
+
+void testfillroundrect(void) {
+  uint8_t color = BLACK;
+  for (int i=0; i<minorHalfSize/2; i+=2) {
+    display.fillRoundRect(i, i, display.width()-2*i, display.height()-2*i, minorHalfSize/2, color&1);
+    display.refresh();
+    color++;
+  }
+}
+
+void testdrawtriangle(void) {
+  for (int i=0; i<minorHalfSize; i+=5) {
+    display.drawTriangle(display.width()/2, display.height()/2-i,
+                     display.width()/2-i, display.height()/2+i,
+                     display.width()/2+i, display.height()/2+i, BLACK);
+    display.refresh();
+  }
+}
+
+void testfilltriangle(void) {
+  uint8_t color = BLACK;
+  for (int i=minorHalfSize; i>0; i-=5) {
+    display.fillTriangle(display.width()/2  , display.height()/2-i,
+                         display.width()/2-i, display.height()/2+i,
+                         display.width()/2+i, display.height()/2+i, color & 1);
+    display.refresh();
+    color++;
+  }
+}
+
+void testdrawchar(void) {
+  display.setTextSize(1);
+  display.setTextColor(BLACK);
+  display.setCursor(0,0);
+  display.cp437(true);
+
+  for (int i=0; i < 256; i++) {
+    if (i == '\n') continue;
+    display.write(i);
+  }
+  display.refresh();
+}
